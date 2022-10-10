1fde6adaeb91527dcc1ec609026b7c0121eed9f3
wcrd <11898748+wcrd@users.noreply.github.com>
2022-10-10 09:51:01 -0600

    - Update imports to be more specific - Create example

--- example.py
+++ example.py
@@ -1,30 +1,22 @@
-import brick_xlsx_validator as bv
+import switch_brick_tools as sbt
 import os
 
-# model_path = r"C:\Users\WillDavidson\Desktop\DB-BrickModelInput-V6.xlsx"
-# model_path = r"C:\Users\WillDavidson\OneDrive - Switch Automation\R&D\Brick Modelling\sites\Carrier CIB\20210728 - Carrier-CiB_subParted _newFormat.xlsx"
-# model_path = r"C:\Users\WillDavidson\OneDrive - Switch Automation\R&D\Brick Modelling\sites\Carrier CIB\20210728 - Carrier-CiB _newFormat.xlsx"
-# model_path = r"C:\Users\WillDavidson\Downloads\eau_clair_equip.xlsx"
-model_path = r"C:\Users\WillDavidson\OneDrive - Switch Automation\R&D\Brick Modelling\sites\Carrier Lerner\20210808 - Carrier-Lerner _newFormat.xlsx"
 
-df, bad_refs, bad_classes, duplicate_ids = bv.validate(os.path.join(model_path), switch_version="1.1.7", reference_field=("Brick", "label"))
+document_filepath = os.path.join("./testing_files/model_v1.xlsx")
 
+## Validation
+bad_rows, bad_refs, bad_classes, duplicate_ids = sbt.validate(document_filepath, switch_version="1.1.7", reference_field=("Brick", "label"))
 
-
-import brick_xlsx_generator as bg
-import os
-
-# xlsx path
-document_filepath = r"PATH TO FILE"
+## Generation
+x = sbt.Dataset()
 
 # rdf model namespacing
 PORTFOLIO = "example_portfolio"
 BUILDING_NAME = "example_building"
 
-# initialise converter
-g = bg.Graph()
-# process input file to generate graph model
-g.process(document_filepath, PORTFOLIO, BUILDING_NAME)
+
+x.process(document_filepath, relationship_field=("Brick", "label"), portfolio_name=PORTFOLIO, building_name=BUILDING_NAME)
+
 # export graph model to TTL file
 g.export()                          # export full model
 g.export(export_mode="building")    # export buidling entities only
--- switch_brick_tools/__init__.py
+++ switch_brick_tools/__init__.py
@@ -1,4 +1,8 @@
+# import submodules to use if needed
 from . import generator, validator
+# import specifics to make life easier
+from .generator import Dataset, Graph
+from .validator import validate
 
 import logging
 
